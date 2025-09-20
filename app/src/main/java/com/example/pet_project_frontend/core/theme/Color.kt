7c82201fb0564fc79fe3521394a4cd0130651667
kingy0ujin <godjin11@naver.com>
2025-09-21 01:17:02 +0900

    style: 피그마 색상 팔레트 적용 및 변수명 수정

--- app/src/main/java/com/example/pet_project_frontend/core/theme/Color.kt
+++ app/src/main/java/com/example/pet_project_frontend/core/theme/Color.kt
@@ -16,4 +16,24 @@ object MyPageColors {
     val ProfileAddButton = Color(0xFFE4E7EA)
     val Scrim = Color(0x52000000) // 32% opacity
     val TapChosen: Color = Color(0xFFFFFFFF)
+
+    // 펫케어 화면
+    val Blue100: Color = Color(0xFFC9E2FF)
+    val Blue800: Color = Color(0xFF1957C2)
+    val Yellow100: Color = Color(0xFFFFEFBF)
+    val Orange800: Color = Color(0xFFFB8800)
+    val Orange600: Color = Color(0xFFED6700)
+    val Green600: Color = Color(0xFF02A262)
+    val Green500: Color = Color(0xFF03B26C)
+    val Grey100: Color = Color(0xFFF2F4F6)
+    val Grey900: Color = Color(0xFF191F28)
+    val Grey600: Color = Color(0xFF6B7684)
+    val Grey500: Color = Color(0xFF8B95A1)
+    val Grey700: Color = Color(0xFF4E5968)
+    val Grey800: Color = Color(0xFF333D4B)
+    val Grey400: Color = Color(0xFFB0B8C1)
+    val Grey300: Color = Color(0xFFD1D6DB)
+    val GreyOpacity600: Color = Color(0x94132B00) // 58%
+    val GreyOpacity200: Color = Color(0x1A1B3700) // 10%
+    val GreyOpacity100: Color = Color(0x0D022047) // 5%
 }
